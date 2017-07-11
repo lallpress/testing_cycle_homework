08ba61938510160ecabfeb7764e370ac45315ad1
Lauren Allpress <lallpress@unomaha.edu>
2017-07-11 01:29:37 +0000

    A little lost with rspec, so I ended up just making the first cucumber scenario green. Probably not the point of this exercise, but it's progress.

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -16,6 +16,14 @@ module NavigationHelpers
     when /^the home\s?page$/
       '/'
 
+    when /^the edit page for "(.+)"$/
+      w=Movie.find_by_title!($1)
+      edit_movie_path(w)
+
+    when /^the details page for "(.+)"$/
+      w=Movie.find_by_title!($1)
+      movie_path(w)
+      
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
     #
